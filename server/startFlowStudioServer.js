90c6b72ed5c7f9c3224b4f739e3e88727112bacb
Maikel van de Lisdonk <maikel@devhelpr.com>
2019-09-20 09:18:02 +0200

    mvdl : added new options : port and flowFileCopies

--- server/startFlowStudioServer.js
+++ server/startFlowStudioServer.js
@@ -1,11 +1,21 @@
 const fs = require('fs');
 const path = require('path');
-function start(flowFileName, metaDataJson) {
+function start(flowFileName, metaDataJson, options) {
+
+	const metaDataInfo = metaDataJson.sort((a, b) => {
+		if (a.fullName < b.fullName) {
+			return -1;
+		}
+		if (a.fullName > b.fullName) {
+			return 1;
+		}
+		return 0;
+	});
 
 	const express = require('express');
 	const bodyParser = require('body-parser');
 	const app = express();
-	const port = 4000;
+	const port = !!options && !!options.port ? options.port : 4000;
 
 	app.set('views', path.join(__dirname, '../views'));
 	app.set('view engine', 'ejs');
@@ -14,9 +24,18 @@ function start(flowFileName, metaDataJson) {
 	
 	app.get('/', (req, res) => res.render('./pages/index'));
 	app.post('/save-flow', (req, res) => {
-
-		fs.writeFileSync(flowFileName, JSON.stringify(req.body));
+		const bodyAsJsonString = JSON.stringify(req.body);
+		
+		fs.writeFileSync(flowFileName, bodyAsJsonString);
+		console.log("Flow file written to:", flowFileName);
 		res.send(JSON.stringify({status:true}));
+
+		if (!!options && !!options.flowFileCopies) {
+			options.flowFileCopies.map((flowFileCopy) => {
+				fs.writeFileSync(flowFileCopy, bodyAsJsonString);
+				console.log("Flow file copied to:", flowFileCopy);
+			});
+		}
 	});
 
 	app.get('/get-flow', (req, res) => {
@@ -27,11 +46,11 @@ function start(flowFileName, metaDataJson) {
 	);
 
 	app.get('/get-tasks', (req, res) => {
-		res.send(JSON.stringify(metaDataJson));
+		res.send(JSON.stringify(metaDataInfo));
 	}
 );
 
-	app.listen(port, () => console.log(`FlowStudio app listening on port ${port}!`))
+	app.listen(port, () => console.log(`FlowCanvas web-app listening on port ${port}!`))
 }
 
 module.exports = {
