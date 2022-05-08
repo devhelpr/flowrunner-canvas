8edc4fde0f89a165eeba25b4f7e98ddf08548efb
Maikel van de Lisdonk <maikel@devhelpr.com>
2022-05-08 10:07:09 +0200

    small css fixes

--- src/components/canvas/canvas-components/html-node.tsx
+++ src/components/canvas/canvas-components/html-node.tsx
@@ -134,8 +134,8 @@ export const HtmlNode = React.forwardRef((props: IHtmlNodeProps, ref) => {
 			className={selected + "canvas__html-shape canvas__html-shape-" + props.node.name + 			
 				nodeState + 
 				(settings.background ? ` ${settings.background} ` : "") + 
-				(FlowToCanvas.getHasOutputs(shapeType, settings) ? "" : "canvas__html-shape--no-outputs") + " " +
-				(FlowToCanvas.getHasInputs(shapeType, settings) ? "" : "canvas__html-shape--no-inputs")
+				(FlowToCanvas.getHasOutputs(shapeType, settings) ? "" : " canvas__html-shape--no-outputs") + " " +
+				(FlowToCanvas.getHasInputs(shapeType, settings) ? "" : " canvas__html-shape--no-inputs")
 				}
 				onMouseUp={(event) => props.onMouseEnd(props.node, event)}
 				>
